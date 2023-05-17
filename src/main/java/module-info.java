cb961385bafe41d5c543ddd64267f8cdb98dd304
Jannik-LS <70889142+Jannik-LS@users.noreply.github.com>
2023-05-17 13:35:18 +0200

    Delete src
    Incorrect storage location

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.fuelstationdatacollector {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.net.http;
-
-
-    opens com.example.fuelstationdatacollector to javafx.fxml;
-    exports com.example.fuelstationdatacollector;
-}
